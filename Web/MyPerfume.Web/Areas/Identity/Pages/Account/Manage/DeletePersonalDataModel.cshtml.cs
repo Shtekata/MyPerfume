b08c0aebec7908e2083a6ca4c61cf474188624fb
Shtekata <gesheval@gmail.com>
2020-04-13 19:49:14 +0300

    DeleteUser

---
+++ Web/MyPerfume.Web/Areas/Identity/Pages/Account/Manage/DeletePersonalDataModel.cshtml.cs
@@ -0,0 +1,95 @@
+﻿namespace MyPerfume.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+    using MyPerfume.Data.Common.Repositories;
+    using MyPerfume.Data.Models;
+
+    public class DeletePersonalDataModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<DeletePersonalDataModel> logger;
+        private readonly IDeletableEntityRepository<ApplicationUser> userRepository;
+
+        public DeletePersonalDataModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<DeletePersonalDataModel> logger,
+            IDeletableEntityRepository<ApplicationUser> userRepository)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.userRepository = userRepository;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public bool RequirePassword { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            this.RequirePassword = await this.userManager.HasPasswordAsync(user);
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            this.RequirePassword = await this.userManager.HasPasswordAsync(user);
+            if (this.RequirePassword)
+            {
+                if (!await this.userManager.CheckPasswordAsync(user, this.Input.Password))
+                {
+                    this.ModelState.AddModelError(string.Empty, "Incorrect password.");
+                    return this.Page();
+                }
+            }
+
+            user.IsDeleted = true;
+            user.UserName = null;
+            user.Email = null;
+            user.NormalizedUserName = null;
+            user.NormalizedEmail = null;
+            user.PhoneNumber = null;
+            var result = await this.userRepository.SaveChangesAsync();
+            var userId = await this.userManager.GetUserIdAsync(user);
+            if (result == 0)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred deleting user with ID '{userId}'.");
+            }
+
+            await this.signInManager.SignOutAsync();
+
+            this.logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
+
+            return this.Redirect("~/");
+        }
+    }
+}
